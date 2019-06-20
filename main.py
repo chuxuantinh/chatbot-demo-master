b022147aa960f6aaeedbb0a5283fa79c7ec58e10
chuxuantinh <chuxuantinhcs11@gmail.com>
2019-06-20 14:47:33 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from ChatBotTraining import trainingChatBot
+from ChatBot import runChatBot
+
+#trainingChatBot()
+runChatBot()
+
+
+
+
+# Epoch = 1000
+# Batch_Size = 8
+# iter = Total_Size / Batch_Size
+# acc =
